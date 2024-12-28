f09cafc1f701fc699cc10c6a4c0802b3b9ca3f97
anamika422 <agarwal.anamika422@gmail.com>
2024-12-27 22:05:20 -0800

    updted db file

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
+require('dotenv').config()
+
 
 const connectToDB = ()=>{
-    mongoose.connect('mongodb://127.0.0.1:27017/JobPortal')
+    mongoose.connect(`mongodb+srv://agarwalanamika422:${process.env.Mongo_Password}@jobportal.0o7gc.mongodb.net/?retryWrites=true&w=majority&appName=jobportal`)
       .then(() => console.log('Connected to mongodb successfully'))
       .catch(() =>console.log('error in connecting mongodb'))
 }
